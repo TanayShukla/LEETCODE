8d5b91852aac872cb2be7a4e6dda1b063ffb9801
Tanay <77578362+TanayShukla@users.noreply.github.com>
2024-05-16 21:06:15 +0530

    Time: 2334 ms (34.68%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,15 @@
 /* Write your T-SQL query statement below */
-SELECT DISTINCT(author_id)  AS id
+/*SELECT DISTINCT(author_id)  AS id
 FROM Views
 WHERE viewer_id = author_id AND viewer_id >=1
-ORDER BY id ASC
+ORDER BY id ASC */
+
+
+select distinct
+    author_id as id
+from
+    views
+where
+    author_id = viewer_id
+order by
+    author_id    
