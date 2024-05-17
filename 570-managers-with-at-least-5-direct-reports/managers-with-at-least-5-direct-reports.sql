fdaf2271b8c01170b13bd673b8da877a9fb48ab2
Tanay <77578362+TanayShukla@users.noreply.github.com>
2024-05-17 16:18:16 +0530

    Time: 1772 ms (31.52%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,7 @@
-/*SELECT e.name
-FROM Employee AS e 
-INNER JOIN Employee AS m ON e.id=m.managerId 
-GROUP BY m.managerId 
-HAVING COUNT(m.managerId) >= 5*/
+select e1.name from employee e1 join employee e2
+on e1.id=e2.managerId
+group by e2.managerId,e1.name
+having count(e2.managerId)>=5
+/*
 SELECT name FROM Employee
-WHERE id in (select managerId from employee group by managerId having count(id) >= 5);
+WHERE id in (select managerId from employee group by managerId having count(id) >= 5);*/
