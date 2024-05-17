c14a1db8db6e9e19bdcb3bba77d88ea53ece8f06
Tanay <77578362+TanayShukla@users.noreply.github.com>
2024-05-17 10:12:17 +0530

    Time: 1223 ms (93.59%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,7 +1,13 @@
-/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below 
 SELECT E.name AS [name] , B.Bonus AS bonus
 FROM Employee E
 LEFT JOIN Bonus B
 ON E.empId = B.empId
 Where B.bonus < 1000 OR B.bonus IS NULL
 ORDER BY bonus DESC;
+*/
+/* Write your T-SQL query statement below */
+SELECT name, bonus
+FROM Employee e
+LEFT JOIN Bonus b ON e.empId = b.empId
+WHERE ISNULL(Bonus, 0) < 1000
