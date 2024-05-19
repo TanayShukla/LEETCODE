97430b73db1eb91ef84a421aa6725b081ec97dc8
Tanay <77578362+TanayShukla@users.noreply.github.com>
2024-05-19 21:31:33 +0530

    Time: 1492 ms (20.54%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,6 +1,19 @@
-SELECT              s.user_id,
+/*SELECT              s.user_id,
 ROUND(CAST(COUNT(CASE WHEN action = 'confirmed' THEN 1 ELSE NULL END) AS FLOAT) / COUNT(*), 2) as confirmation_rate
 FROM                Signups s
 LEFT JOIN     Confirmations c
 ON                  s.user_id = c.user_id
 GROUP BY            s.user_id
+*/
+/* Write your T-SQL query statement below */
+SELECT s.user_id, ISNULL(ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0.0 END), 2.0), 0.0) AS confirmation_rate
+FROM (
+    SELECT DISTINCT user_id
+    FROM Signups
+    UNION
+    SELECT DISTINCT user_id
+    FROM Confirmations
+) AS s
+LEFT JOIN Confirmations AS c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
