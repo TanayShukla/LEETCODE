e99659fc63e059bb3fb74293dc2aa84b66f0a13f
Tanay <77578362+TanayShukla@users.noreply.github.com>
2024-05-20 07:41:15 +0530

    Time: 952 ms (32.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT
+    P.product_id,
+    ISNULL(ROUND(SUM(P.price * US.units * 1.0) / SUM(US.units), 2), 0) AS average_price 
+FROM Prices P
+    LEFT LOOP JOIN UnitsSold US ON US.product_id = P.product_id 
+        AND US.purchase_date BETWEEN P.start_date AND P.end_date
+GROUP BY P.product_id       
+            
