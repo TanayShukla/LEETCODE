811ad175548840ad63d1617f222e13605aa9fc49
Tanay <77578362+TanayShukla@users.noreply.github.com>
2024-05-20 07:42:03 +0530

    Time: 703 ms (78.60%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
+/*
 SELECT
     P.product_id,
     ISNULL(ROUND(SUM(P.price * US.units * 1.0) / SUM(US.units), 2), 0) AS average_price 
@@ -6,4 +7,15 @@ FROM Prices P
     LEFT LOOP JOIN UnitsSold US ON US.product_id = P.product_id 
         AND US.purchase_date BETWEEN P.start_date AND P.end_date
 GROUP BY P.product_id       
-            
+*/
+/* Write your T-SQL query statement below */
+select 
+    prices.product_id, 
+    case
+        when round(sum(units * price * 1.00)/sum(units * 1.00), 2) IS NULL
+        then 0
+        else round(sum(units * price * 1.00)/sum(units * 1.00), 2) 
+    end as average_price
+from prices
+left join unitssold on (unitsSold.purchase_date between prices.start_date and prices.end_date) and Unitssold.product_id = prices.product_id
+group by prices.product_id
